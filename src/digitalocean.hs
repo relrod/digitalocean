b26ca9197e8d050e25a658e63efd77f3508fbd83
Maxwell Swadling <maxwellswadling@gmail.com>
2013-12-01 10:52:51 +1100

    Added sizes
    Plus a refactor of responses

--- src/digitalocean.hs
+++ src/digitalocean.hs
@@ -1,18 +1,24 @@
-{-# LANGUAGE OverloadedStrings, RankNTypes #-}
+{-# LANGUAGE OverloadedStrings, RankNTypes, ScopedTypeVariables #-}
 -- BSD-3, Ricky Elrod <ricky@elrod.me>
 
 module Network.DigitalOcean (
   Authentication (..),
-  droplets
+  droplets,
+  regions,
+  sizes
 ) where
 
 import Control.Applicative ((<$>), (<*>))
 import Control.Monad (liftM)
+import Data.Text (Text, unpack)
 import Control.Monad.IO.Class
 import Data.Aeson ((.:), (.:?), decode, FromJSON(..), Value(..))
 import qualified Data.ByteString.Lazy.Char8 as BS
 import Network.HTTP.Conduit
+import Data.Monoid
 import Network.HTTP.Base (urlEncode)
+import Data.Aeson.Types (Parser)
+import Debug.Trace
 
 data Authentication = Authentication { clientId :: String, apiKey ::  String } deriving (Show)
 
@@ -29,26 +35,47 @@ data Droplet = Droplet {
   createdAt :: String
 } deriving (Show)
 
-data DropletsResponse = DropletsResponse {
-  status :: String,
-  rDroplets :: [Droplet]
-} deriving (Show)
-
-data RegionsResponse = RegionsResponse {
-  rResponseStatus :: String,
-  rRegions :: [Region]
-} deriving (Show)
-
 data Region = Region {
   rId :: Int,
   rName :: String
 } deriving (Show)
 
-instance FromJSON DropletsResponse where
+data Size = Size {
+  sId :: Int,
+  sName :: String,
+  sMemory :: Int,
+  sCpu :: Int,
+  sDisk :: Int,
+  sCostHour :: Int,
+  sCostMonth :: String -- Yeah, it's a string.
+} deriving (Show)
+
+data DOResponse a = DOResponse {
+  rResponseStatus :: String,
+  rResponseObjects :: [a]
+} deriving (Show)
+
+type DropletsResponse = DOResponse Droplet
+type RegionsResponse = DOResponse Region
+type SizesResponse = DOResponse Size
+
+class DOResp a where
+  primaryKey :: a -> Text
+  request :: FromJSON a => Authentication -> (MonadIO m) => m (Maybe (DOResponse a))
+  request x = liftM decode $ simpleHttp $ constructURL ("/" <> unpack (primaryKey (undefined :: a)) <> "/") x
+
+instance DOResp Region where
+  primaryKey _ = "regions"
+instance DOResp Droplet where
+  primaryKey _ = "droplets"
+instance DOResp Size where
+  primaryKey _ = "sizes"
+
+instance (DOResp a, FromJSON a) => FromJSON (DOResponse a) where
   parseJSON (Object v) =
-    DropletsResponse <$>
+    DOResponse <$>
     (v .: "status") <*>
-    (v .: "droplets")
+    (v .: primaryKey (undefined :: a))
 
 instance FromJSON Droplet where
   parseJSON (Object v) =
@@ -64,17 +91,22 @@ instance FromJSON Droplet where
     (v .: "status") <*>
     (v .: "created_at")
 
-instance FromJSON RegionsResponse where
-  parseJSON (Object v) =
-    RegionsResponse <$>
-    (v .: "status") <*>
-    (v .: "regions")
-
 instance FromJSON Region where
   parseJSON (Object v) =
     Region <$>
     (v .: "id") <*>
     (v .: "name")
+
+instance FromJSON Size where
+  parseJSON (Object v) =
+    Size <$>
+    (v .: "id") <*>
+    (v .: "name") <*>
+    (v .: "memory") <*>
+    (v .: "cpu") <*>
+    (v .: "disk") <*>
+    (v .: "cost_per_hour") <*>
+    (v .: "cost_per_month")
 
 -- The API url
 url :: String
@@ -87,10 +119,11 @@ authQS a = "client_id=" ++ urlEncode (clientId a) ++ "&api_key=" ++ urlEncode (a
 constructURL :: String -> Authentication -> String
 constructURL a b = url ++ a ++ "?" ++ authQS b
 
--- GET /droplets
 droplets :: Authentication -> (MonadIO m) => m (Maybe DropletsResponse)
-droplets a = liftM decode $ simpleHttp $ constructURL "/droplets" a
+droplets = request
 
--- GET /regions
 regions :: Authentication -> (MonadIO m) => m (Maybe RegionsResponse)
-regions a = liftM decode $ simpleHttp $ constructURL "/regions" a
+regions = request
+
+sizes :: Authentication -> (MonadIO m) => m (Maybe SizesResponse)
+sizes = request
